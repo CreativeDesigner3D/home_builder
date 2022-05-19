ce7f84ce0d351b05f0b1989cd8d2723a8402e959
Andrew Peel <34818078+CreativeDesigner3D@users.noreply.github.com>
2022-05-19 16:53:14 -0700

    Implement Fixture Data

--- asset_libraries/sample_fixtures/__init__.py
+++ asset_libraries/sample_fixtures/__init__.py
@@ -1,13 +1,14 @@
 import os
-
+from . import drop_ops_fixtures
 LIBRARY_TYPE = 'FIXTURES'
 FIXTURES_LIBRARY_PATH = os.path.join(os.path.dirname(__file__),'library',"Sample Fixtures")
 
 FIXTURES = {"library_name": "Sample Fixtures",
             "library_type": "FIXTURES",
-            "library_path": FIXTURES_LIBRARY_PATH}
+            "library_path": FIXTURES_LIBRARY_PATH,
+            "libary_drop_id": "hb_sample_fixtures.drop_fixture"}
             
 LIBRARIES = [FIXTURES]
 
 def register():
-    pass
+    drop_ops_fixtures.register()
