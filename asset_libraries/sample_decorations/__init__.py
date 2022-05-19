7a27784c30fc9afaf95c4f0d09bfab46471ecaf8
Andrew Peel <34818078+CreativeDesigner3D@users.noreply.github.com>
2022-05-19 09:36:06 -0700

    Adding Place Deco Operator

--- asset_libraries/sample_decorations/__init__.py
+++ asset_libraries/sample_decorations/__init__.py
@@ -1,18 +1,20 @@
 import os
-
+from . import ops_sample_deco
 KITCHEN_DECO_PATH = os.path.join(os.path.dirname(__file__),'library',"Kitchen Decorations")
 ROOM_DECO_PATH = os.path.join(os.path.dirname(__file__),'library',"Room Decorations")
 
 SAMPLE_KITCHEN_DECORATIONS = {"library_name": "Sample Kitchen Decorations",
                               "library_type": "DECORATIONS",
-                              "library_path": KITCHEN_DECO_PATH}
+                              "library_path": KITCHEN_DECO_PATH,
+                              "libary_drop_id": "home_builder.place_decoration"}
 
 SAMPLE_ROOM_DECORATIONS = {"library_name": "Sample Room Decorations",
                            "library_type": "DECORATIONS",
-                           "library_path": ROOM_DECO_PATH}
+                           "library_path": ROOM_DECO_PATH,
+                           "libary_drop_id": "home_builder.place_decoration"}
 
 LIBRARIES = [SAMPLE_KITCHEN_DECORATIONS,
              SAMPLE_ROOM_DECORATIONS]
 
 def register():
-    pass
+    ops_sample_deco.register()
---
+++ python_libs/pc_lib/pc_placement_utils.py
@@ -0,0 +1,22 @@
+
+def event_is_place_asset(event):
+    if event.type == 'LEFTMOUSE' and event.value == 'PRESS':
+        return True
+    elif event.type == 'NUMPAD_ENTER' and event.value == 'PRESS':
+        return True
+    elif event.type == 'RET' and event.value == 'PRESS':
+        return True
+    else:
+        return False
+
+def event_is_cancel_command(event):
+    if event.type in {'RIGHTMOUSE', 'ESC'}:
+        return True
+    else:
+        return False
+
+def event_is_pass_through(event):
+    if event.type in {'MIDDLEMOUSE', 'WHEELUPMOUSE', 'WHEELDOWNMOUSE'}:
+        return True
+    else:
+        return False
