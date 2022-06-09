28c99f760a2cf7537a490fdb078cef32c8c6a12a
Andrew Peel <34818078+CreativeDesigner3D@users.noreply.github.com>
2022-06-09 14:50:01 -0700

    Updating Door and Window UI

--- asset_libraries/sample_doors_windows/ui_doors_windows.py
+++ asset_libraries/sample_doors_windows/ui_doors_windows.py
@@ -19,21 +19,37 @@ class HOME_BUILDER_PT_door_window_parts(bpy.types.Panel):
         layout = self.layout
         layout.label(text="Door and Window Parts")        
         props = utils_doors_windows.get_scene_props(context.scene)
-        row = layout.row()
+
+        box = layout.box()
+        box.label(text="Door Parts")
+
+        row = box.row()
+        row.label(text="Door Panel")
+        row.label(text="Door Frame")
+        row.label(text="Door Handle")
+
+        row = box.row()
         row.prop(props,'entry_door_panel_category',text="")
         row.prop(props,'entry_door_frame_category',text="")
         row.prop(props,'entry_door_handle_category',text="")
 
-        row = layout.row()
+        row = box.row()
         row.template_icon_view(props,"entry_door_panel",show_labels=True)   
         row.template_icon_view(props,"entry_door_frame",show_labels=True)   
         row.template_icon_view(props,"entry_door_handle",show_labels=True)   
 
-        row = layout.row()
+        box = layout.box()
+        box.label(text="Window Parts")
+
+        row = box.row()
+        row.label(text="Window Frame")
+        row.label(text="Window Insert")
+
+        row = box.row()
         row.prop(props,'window_frame_category',text="")
         row.prop(props,'window_insert_category',text="")
 
-        row = layout.row()
+        row = box.row()
         row.template_icon_view(props,"window_frame",show_labels=True)   
         row.template_icon_view(props,"window_insert",show_labels=True)  
 
